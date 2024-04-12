bdcd7e7718ff18d669926aea6853388e00c13cd8
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-04-12 03:21:48 +0100

    first commit

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,3 @@
+# 0x00. Personal data
+
+This project is on PII (Personal Identifiable Information) and Logging.
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Filtered logger"""
+import logging
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                      field + "=" + redaction + separator, message)
+    
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Constructor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters incoming log records"""
+        message = filter_datum(
+            self.fields, self.REDACTION, super(
+                RedactingFormatter, self).format(record),
+            self.SEPARATOR)
+        
+        return message

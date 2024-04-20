80874397026e315e532481b4587288c1aa6f193e
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-04-21 00:10:51 +0100

    touches

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,7 +17,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 if getenv("AUTH_TYPE") == 'basic_auth':
-    from app.v1.auth.basic_auth import BasicAuth
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 else:
     from api.v1.auth.auth import Auth

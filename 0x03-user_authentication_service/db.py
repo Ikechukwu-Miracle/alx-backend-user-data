f880ae8bba41d1d66f58721fc8306ddfc531620b
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-04-24 23:52:22 +0100

    get_user_by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -29,19 +31,32 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """Method that creates a User object
-        
+
         Args:
             email (str): email of the user
             hashed_password (str): password hashed for security
-            
+
         Returns:
             A User object
         """
         newUser = User(email=email, hashed_password=hashed_password)
-        self._session.add(newUser)
+        self_session.add(newUser)
         self._session.commit()
-        
+
         return newUser
+
+    def find_user_by(self, **kwargs) -> User:
+        """Returns a user based on keyword"""
+        try:
+            result = self._session.query(User).filter_by(**kwargs).first()
+
+            if result is None:
+                raise NoResultFound
+
+        except TypeError:
+            raise InvalidRequestError
+
+        return result

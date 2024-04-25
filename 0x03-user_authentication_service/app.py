7f8bd4627612d18578784a12d81b73f238b6a1d2
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-04-25 17:29:29 +0100

    profile, login and logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,9 +58,12 @@ def login() -> str:
 def logout():
     """Ends a session"""
     cookie = request.cookies.get("session_id", None)
+    if  cookie is None:
+        abort(403)
+
     user = AUTH.get_user_from_session_id(cookie)
 
-    if cookie is None and user is None:
+    if user is None:
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect("/")

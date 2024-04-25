541a1d48e10b1afccf92697e8859b79643ee1d52
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-04-25 13:05:23 +0100

    session_id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -13,6 +13,7 @@ def index() -> str:
     """JSON message"""
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route("/users", methods=['POST'], strict_slashes=False)
 def users() -> str:
     """POST /users
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,12 +4,17 @@ from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
     """hashes password using bcript.hashpw"""
 
     return hashpw(password.encode('utf-8'), gensalt())
+
+def _generate_uuid() -> str:
+    """Generates a unique identification number"""
+    return str(uuid4())
 
 
 class Auth:
@@ -30,3 +35,21 @@ class Auth:
             hashed_password = _hash_password(password)
             newUser = self._db.add_user(email, hashed_password)
             return newUser
+        
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
+    
+    def create_session(self, email: str) -> str:
+        """Creates a new session_id"""
+        try:
+            user = self._db.find_user_by(email=email)
+            ses_id = _generate_uuid()
+            user.session_id = ses_id
+            return ses_id
+        except NoResultFound:
+            return None

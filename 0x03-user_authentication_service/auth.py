47d025a429db26d8f074c65baf698c5e5250c1d4
Ikechukwu-Miracle <nwekeiyke46@gmail.com>
2024-04-26 04:04:34 +0100

    finishing touches

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,4 +94,5 @@ class Auth:
             raise ValueError
 
         hashed_password = _hash_password(password)
-        self._db.update_user(user.id, password=hashed_password, reset_token=None)
+        self._db.update_user(user.id,
+                             password=hashed_password, reset_token=None)
